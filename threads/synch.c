e5498f85732c22dbe53ed4b2e616070d00441247
DongHa Kim <summitkim@kaist.ac.kr>
2020-04-03 20:39:06 +0900

    sync 2nd again

--- threads/synch.c
+++ threads/synch.c
@@ -117,7 +117,7 @@ sema_up (struct semaphore *sema) {
 	}
 	sema->value++;
 
-	test_max_priority();														/* priority preemption */
+	thread_yield();														/* priority preemption */
 
 	intr_set_level (old_level);
 }
