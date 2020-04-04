454097a60cbb886fa8eda7469a9ac3348f1475f3
DongHa Kim <summitkim@kaist.ac.kr>
2020-04-04 21:32:39 +0900

    set fixed

--- threads/thread.c
+++ threads/thread.c
@@ -320,7 +320,6 @@ thread_yield (void) {
 /* Sets the current thread's priority to NEW_PRIORITY. */
 void
 thread_set_priority (int new_priority) {
-	thread_current ()->priority = new_priority;
 	thread_current ()->init_priority = new_priority;
 	refresh_priority();
 	donate_priority();
