519b3dd9e89dfb1e7193bbd2669cc3e90ec6b992
DongHa Kim <summitkim@kaist.ac.kr>
2020-04-06 01:58:46 +0900

    exp

--- threads/thread.c
+++ threads/thread.c
@@ -478,7 +478,7 @@ init_thread(struct thread* t, const char* name, int priority) {
    strlcpy(t->name, name, sizeof t->name);
    t->tf.rsp = (uint64_t)t + PGSIZE - sizeof(void*);
    t->priority = priority;
-   t->recent_cpu = RECENT_CPU_DEFAULT;
+   t->recent_cpu = RECENT_CPU_DEFAULT + 1;
    t->nice = NICE_DEFAULT;
 
    t->magic = THREAD_MAGIC;
