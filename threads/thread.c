e8421beb60c35802be1b7259da29f8ede5566555
DongHa Kim <summitkim@kaist.ac.kr>
2020-05-02 20:50:49 +0900

    asd

--- threads/thread.c
+++ threads/thread.c
@@ -656,7 +656,7 @@ schedule(void) {
 //      if (curr && curr->status == THREAD_DYING && curr != initial_thread) {
 //         ASSERT(curr != next);
 //         list_push_back(&destruction_req, &curr->elem);
-      }
+//      }
 
       /* Before switching the thread, we first save the information
        * of current running. */
