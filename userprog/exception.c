58f398989b2247abe071db329ad6d9abf84254cc
DongHa Kim <summitkim@kaist.ac.kr>
2020-05-03 17:31:01 +0900

    asdf

--- userprog/exception.c
+++ userprog/exception.c
@@ -140,6 +140,8 @@ page_fault (struct intr_frame *f) {
 	write = (f->error_code & PF_W) != 0;
 	user = (f->error_code & PF_U) != 0;
 
+	exit(-1);
+
 #ifdef VM
 	/* For project 3 and later. */
 	if (vm_try_handle_fault (f, fault_addr, user, write, not_present))
@@ -156,6 +158,5 @@ page_fault (struct intr_frame *f) {
 			write ? "writing" : "reading",
 			user ? "user" : "kernel");
 	kill (f);
-	exit(-1);
 }
 
