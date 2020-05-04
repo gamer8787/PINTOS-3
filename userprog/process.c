30402019919a5f163628602d7446b351b286a32d
DongHa Kim <summitkim@kaist.ac.kr>
2020-05-04 18:02:47 +0900

    asdf

--- userprog/process.c
+++ userprog/process.c
@@ -99,7 +99,7 @@ duplicate_pte (uint64_t *pte, void *va, void *aux) {
 	bool writable;
 
 	/* 1. TODO: If the parent_page is kernel page, then return immediately. */
-	if (is_kernel_vaddr(parent_page))
+	if (is_kern_pte((uint64_t *)va))
 	{
 		return false;
 	}
