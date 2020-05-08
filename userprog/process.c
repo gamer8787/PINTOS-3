f9f3c850a39e6191c3af5f31c17655571df64e6a
DongHa Kim <summitkim@kaist.ac.kr>
2020-05-08 18:23:12 +0900

    malloc

--- userprog/process.c
+++ userprog/process.c
@@ -287,7 +287,8 @@ process_exit (void) {
 		}
 	}
 	curr->next_fd = 3;
-	palloc_free_page(curr->fdt);
+	free(curr->fdt);
+	//palloc_free_page(curr->fdt);
 	file_close(curr->run_file);
 	process_cleanup ();
 }
